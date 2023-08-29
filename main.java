99be439720e5bf82e391549c15844271bbe446a8
Antônio Venâncio <114105731+nettodmlo@users.noreply.github.com>
2023-08-28 21:19:18 -0300

    Add files via upload

--- main.java
+++ main.java
@@ -2,54 +2,23 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
+        Scanner sc = new Scanner(System.in);
+        Media media = new Media();
 
-        while (true) {
-            System.out.println("Choose an operation:");
-            System.out.println("1. Addition");
-            System.out.println("2. Subtraction");
-            System.out.println("3. Division");
-            System.out.println("4. Multiplication");
-            System.out.println("0. Exit");
-            int choice = scanner.nextInt();
+        System.out.println("Type your name: ");
+        media.setName(sc.nextLine());
+        System.out.println("Send me your first grade: ");
+        media.setX(sc.nextInt());
+        System.out.println("Send me your second grade: ");
+        media.setY(sc.nextInt());
 
-            if (choice == 0) {
-                System.out.println("Exiting the calculator.");
-                break;
-            }
+        double average = media.calculateAverage();
+        System.out.println("Average: " + average);
 
-            if (choice < 1 || choice > 4) {
-                System.out.println("Invalid choice. Please select a valid option.");
-                continue;
-            }
-
-            System.out.print("Enter the first number: ");
-            double num1 = scanner.nextDouble();
-
-            System.out.print("Enter the second number: ");
-            double num2 = scanner.nextDouble();
-
-            double result = 0;
-
-            switch (choice) {
-                case 1:
-                    result = calculator.add(num1, num2);
-                    break;
-                case 2:
-                    result = calculator.subtract(num1, num2);
-                    break;
-                case 3:
-                    result = calculator.divide(num1, num2);
-                    break;
-                case 4:
-                    result = calculator.multiply(num1, num2);
-                    break;
-            }
-
-            System.out.println("Result: " + result);
+        if (average >= 7) {
+            System.out.println("Congratulations, " + media.getName() + "! You are approved.");
+        } else {
+            System.out.println("Sorry, " + media.getName() + ". You are not approved.");
         }
-
-        scanner.close();
     }
 }

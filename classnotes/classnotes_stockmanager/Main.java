a72d2c28217a0ed49be9a9ad193d784cd0ea7333
Antônio Venâncio <114105731+nettodmlo@users.noreply.github.com>
2023-09-09 01:56:36 -0300

    Add files via upload

--- classnotes/classnotes_stockmanager/Main.java
+++ classnotes/classnotes_stockmanager/Main.java
@@ -2,16 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Parte de cadastro dos produtos
         Scanner sc = new Scanner(System.in);
         Stock stock = new Stock();
-        System.out.println("Olá! sou o Techbot, seu suporte virtual e irei lhe auxiliar a partir daqui");
-        System.out.println("Vamos lá: digite um código para ser cadastrado. ");
-        int code = sc.nextInt();
+
+        System.out.println("Olá! sou o Techbot, seu suporte virtual e irei lhe auxiliar a partir daqui\n");
+        System.out.println("Vamos lá: digite um código para ser cadastrado.");
+        stock.setScCodigo(sc.nextInt());
         sc.nextLine();
-        System.out.println("Certo! Agora, me dê o nome do produto, por favor. ");
-        String nome = sc.nextLine();
-        System.out.println("Ótimo! Última etapa, forneça a quantidade de itens! ");
-        int quanti = sc.nextInt();
+        System.out.println("Certo! Agora, me dê o nome do produto, por favor.");
+        stock.setScNome(sc.nextLine());
+        System.out.println("Ótimo! Última etapa, forneça a quantidade de itens!");
+        stock.setScQuantidade(sc.nextInt());
+
+        System.out.println("Produto sendo cadastrado...\n");
+        stock.scNome();
+
+        System.out.println("Deseja realizar alguma venda do produto cadastrado? \n Digite 1 = Sim\n Digite 0 = Não");
+
+        //Switch de escolhas: sim ou não
+        int option;
+        do {
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("A opção número um foi escolhida! \n");
+                    System.out.println("Quantos você deseja vender? ");
+                    stock.setScVenda(sc.nextInt());
+                    System.out.println("Venda realizada com sucesso! Itens dessa categoria restante em estoque: " + stock.scVenda());
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida! apenas 1 ou 0 por favor.");
+            }
+
+        } while (option > 1);
 
     }
 }
--- classnotes/classnotes_stockmanager/Stock.java
+++ classnotes/classnotes_stockmanager/Stock.java
@@ -3,16 +3,16 @@ public class Stock {
     private String scNome;
     private int scQuantidade;
 
-    public Stock() {
+    public double getScVenda() {
+        return scVenda;
     }
 
-    public Stock(int scCodigo, String scNome, int scQuantidade) {
-        this.scCodigo = scCodigo;
-        this.scNome = scNome;
-        this.scQuantidade = scQuantidade;
+    public void setScVenda(double scVenda) {
+        this.scVenda = scVenda;
     }
 
-        //getters and setters
+    private double scVenda;
+
     public int getScCodigo() {
         return scCodigo;
     }
@@ -36,4 +36,14 @@ public class Stock {
     public void setScQuantidade(int scQuantidade) {
         this.scQuantidade = scQuantidade;
     }
+
+    // código responsável por exibir produto cadastrado e quantidade de itens em estoque
+    public void scNome () {
+        System.out.println("Parabéns! O produto " + scNome + " foi cadastrado com sucesso. \n");
+    }
+
+    public double scVenda() {
+        return scQuantidade - scVenda;
+    }
+
 }
